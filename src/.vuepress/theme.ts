bdc28c45bbe1678f8e42a84e26e4fb837fa867f9
DengFengLai <3139373986@qq.com>
2024-02-13 00:43:27 +0800

    DF.

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -60,64 +60,21 @@
  ​  ​plugins​: ​{ 
  ​    ​blog​: ​true​, 
   
- ​    ​// install ​@waline​/client before enabling it 
- ​    ​// WARNING: This is a test server for demo only. 
- ​    ​// You should create and use your own comment service in production. 
- ​    ​// comment: { 
- ​    ​//   provider: "Waline", 
- ​    ​//   serverURL: "https://waline-comment.vuejs.press", 
- ​    ​// }, 
-  
- ​    ​// all features are enabled for demo, only preserve features you need here 
- ​    ​mdEnhance​: ​{ 
+ ​  ​mdEnhance​: ​{ 
  ​      ​align​: ​true​, 
  ​      ​attrs​: ​true​, 
-  
- ​      ​// install chart.js before enabling it 
- ​      ​// chart: true, 
-  
  ​      ​codetabs​: ​true​, 
-  
- ​      ​// insert component easily 
- ​      ​// component: true, 
-  
- ​      ​demo​: ​true​, 
-  
- ​      ​// install echarts before enabling it 
- ​      ​// echarts: true, 
-  
+       ​demo​: ​true​, 
  ​      ​figure​: ​true​, 
-  
- ​      ​// install flowchart.ts before enabling it 
- ​      ​// flowchart: true, 
-  
- ​      ​// gfm requires mathjax-full to provide tex support 
- ​      ​// gfm: true, 
-  
  ​      ​imgLazyload​: ​true​, 
  ​      ​imgSize​: ​true​, 
  ​      ​include​: ​true​, 
-  
- ​      ​// install katex before enabling it 
- ​      ​// katex: true, 
-  
- ​      ​// install mathjax-full before enabling it 
- ​      ​// mathjax: true, 
-  
  ​      ​mark​: ​true​, 
-  
- ​      ​// install mermaid before enabling it 
- ​      ​// mermaid: true, 
-  
+ 
  ​      ​playground​: ​{ 
  ​        ​presets​: ​[​"ts"​,​ ​"vue"​]​, 
  ​      ​}​, 
-  
- ​      ​// install reveal.js before enabling it 
- ​      ​// revealJs: { 
- ​      ​//   plugins: ["highlight", "math", "search", "notes", "zoom"], 
- ​      ​// }, 
-  
+ 
  ​      ​stylize​: ​[ 
  ​        ​{ 
  ​          ​matcher​: ​"Recommended"​, 
@@ -135,66 +92,6 @@
  ​      ​sup​: ​true​, 
  ​      ​tabs​: ​true​, 
  ​      ​vPre​: ​true​, 
-  
- ​      ​// install ​@vue​/repl before enabling it 
- ​      ​// vuePlayground: true, 
- ​    ​}​, 
-  
- ​    ​// uncomment these if you want a PWA 
- ​    ​// pwa: { 
- ​    ​//   favicon: "/favicon.ico", 
- ​    ​//   cacheHTML: true, 
- ​    ​//   cachePic: true, 
- ​    ​//   appendBase: true, 
- ​    ​//   apple: { 
- ​    ​//     icon: "/assets/icon/apple-icon-152.png", 
- ​    ​//     statusBarColor: "black", 
- ​    ​//   }, 
- ​    ​//   msTile: { 
- ​    ​//     image: "/assets/icon/ms-icon-144.png", 
- ​    ​//     color: "#ffffff", 
- ​    ​//   }, 
- ​    ​//   manifest: { 
- ​    ​//     icons: [ 
- ​    ​//       { 
- ​    ​//         src: "/assets/icon/chrome-mask-512.png", 
- ​    ​//         sizes: "512x512", 
- ​    ​//         purpose: "maskable", 
- ​    ​//         type: "image/png", 
- ​    ​//       }, 
- ​    ​//       { 
- ​    ​//         src: "/assets/icon/chrome-mask-192.png", 
- ​    ​//         sizes: "192x192", 
- ​    ​//         purpose: "maskable", 
- ​    ​//         type: "image/png", 
- ​    ​//       }, 
- ​    ​//       { 
- ​    ​//         src: "/assets/icon/chrome-512.png", 
- ​    ​//         sizes: "512x512", 
- ​    ​//         type: "image/png", 
- ​    ​//       }, 
- ​    ​//       { 
- ​    ​//         src: "/assets/icon/chrome-192.png", 
- ​    ​//         sizes: "192x192", 
- ​    ​//         type: "image/png", 
- ​    ​//       }, 
- ​    ​//     ], 
- ​    ​//     shortcuts: [ 
- ​    ​//       { 
- ​    ​//         name: "Demo", 
- ​    ​//         short_name: "Demo", 
- ​    ​//         url: "/demo/", 
- ​    ​//         icons: [ 
- ​    ​//           { 
- ​    ​//             src: "/assets/icon/guide-maskable.png", 
- ​    ​//             sizes: "192x192", 
- ​    ​//             purpose: "maskable", 
- ​    ​//             type: "image/png", 
- ​    ​//           }, 
- ​    ​//         ], 
- ​    ​//       }, 
- ​    ​//     ], 
- ​    ​//   }, 
- ​    ​// }, 
+ ​    ​}​,
  ​  ​}​, 
  ​}​)​;
